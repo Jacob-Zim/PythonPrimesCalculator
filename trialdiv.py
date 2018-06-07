e82352fe4ee69152bbe856eb3f01bc757510af38
JacobAZimmerman <ijacobzim@gmail.com>
2018-06-07 10:07:23 -0700

    trial division

---
+++ trialdiv.py
@@ -0,0 +1,16 @@
+import math
+
+def prime(n):
+
+    if n <= 1:
+        return False
+    
+    # slightly heuristic - sqrt(n) will provide all the factors we need
+    # as the factors will simply flip and repeat past that, therefore uneccessary to check
+    for i in range(2, math.ceil(math.sqrt(n))):
+        if n % i == 0:
+            return False
+    
+    return True
+
+print(prime(18))
